aa1f227fae3972b776d1b121f57030163d4c538a
Spencer Kittleson <spencerkittleson@hotmail.com>
2023-04-27 17:05:17 -0700

    updated tests

--- hourAngle.py
+++ hourAngle.py
@@ -2,15 +2,17 @@ from astropy.coordinates import EarthLocation, AltAz, get_sun
 from astropy.time import Time
 from datetime import datetime, timezone
 
-def getSunPosition(latitude, longitude, elevation, observedTime):
-    """Returns the """
+
+def getSunPosition(latitude: float, longitude: float, elevation: float, observedTime: str):
+    """Returns the azimuth and altitude"""
+
     # Define EarthLocation object
-    sd_location = EarthLocation(lat=str(latitude) + ' deg', lon=str(longitude) + ' deg', height=elevation)
+    sd_location = EarthLocation(
+        lat=str(latitude) + ' deg', lon=str(longitude) + ' deg', height=elevation)
 
-    observedTime = str(datetime.now())
-
-    #currentLocalTime = "2023-04-06 13:53:44.284268"
-    currentUtcTime = datetime.fromisoformat(observedTime).astimezone(timezone.utc)
+    # currentLocalTime = "2023-04-06 13:53:44.284268"
+    currentUtcTime = datetime.fromisoformat(
+        observedTime).astimezone(timezone.utc)
 
     # Get the current time
     now = Time(currentUtcTime)
@@ -20,9 +22,4 @@ def getSunPosition(latitude, longitude, elevation, observedTime):
 
     # Convert the Sun's position to altitude and azimuth coordinates
     alt_az = sun.transform_to(AltAz(obstime=now, location=sd_location))
-
-    print(round(alt_az.az.to_value()), round(alt_az.alt.to_value()))
-    
-if __name__ == '__main__':
-    getSunPosition(32.715736, -117.161087, 0, "2023-03-31 14:49:44.284268")
-#getSunPosition(32.715736, -117.161087, 0, "2023-04-06 13:53:44.284268")
+    return (round(alt_az.az.to_value()), round(alt_az.alt.to_value()))
